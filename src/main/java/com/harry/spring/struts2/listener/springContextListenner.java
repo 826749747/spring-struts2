e6406c36f3a0428c48f7bc2387cf0c739a1341b7
LUOHA2 <HARRY.LUO@CARGOSMART.COM>
2018-08-23 19:19:58 +0800

    servlet
    ss

--- src/main/java/com/harry/spring/struts2/listener/springContextListenner.java
+++ src/main/java/com/harry/spring/struts2/listener/springContextListenner.java
@@ -1,7 +1,12 @@
 package com.harry.spring.struts2.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.omg.CORBA.CTX_RESTRICT_SCOPE;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Application Lifecycle Listener implementation class springContextListenner
@@ -27,7 +32,15 @@ public class springContextListenner implements ServletContextListener {
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
      */
     public void contextInitialized(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
+    	//获取Spring配置文件的名称
+    	ServletContext servletContext = sce.getServletContext();
+    	String config = servletContext.getInitParameter("configLocation");
+    	
+        //1. 创建IOC 容器
+    	ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
+    	
+    	//2. 把IOC 容器放在ServletContext的一个属性中
+    	servletContext.setAttribute("ApplicationContext", ctx);
     }
 	
 }
---
+++ src/main/java/com/harry/spring/struts2/servlets/TestServlet.java
@@ -0,0 +1,37 @@
+package com.harry.spring.struts2.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+
+import com.harry.spring.struts2.bean.Person;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//1. 从application域对象中得到IOC容器的应用
+		ServletContext servletContext = getServletContext();
+		ApplicationContext ctx = (ApplicationContext) servletContext.getAttribute("AppcationContext"); 
+		
+		//2.从IOC容器中得到需要的bean
+		Person person = ctx.getBean(Person.class);
+		person.hello();
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
